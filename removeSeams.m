2347ac9f2a80ad40480e435223437faa541c5512
ChuckLeeQB7 <chuckl0416@yahoo.com.tw>
2015-06-27 23:43:15 +0800

    Update removeSeams.m

--- removeSeams.m
+++ removeSeams.m
@@ -1,13 +1,21 @@
-function removeSeams(img, num, obj, Gimg)
+function removeSeams(img, rnum, cnum, obj, Gimg)
 	%remove num of seams in img
 	%if obj is true
-	% the energymap will be Gimg
+	% the energy map will be Gimg
 	% otherwise calculate the energy map
+    num = rnum + cnum;
 	if not(obj)
 		for i=1:num
 			Gimg = getEnergy(img);
-			vec = FindSeam(img, Gimg);
-			img = DeleteSeam(vec, img);
+            [test, decide] = decide(img, Gimg, rnum, cnum);
+			vec = FindSeam(img, Gimg, decide, test);
+            if(decide == 2)
+                cnum = cnum-1;
+            end
+            if(decide == 1)
+                rnum = rnum-1;
+            end
+            img = DeleteSeam(vec, img, decide);
 		end
 	else
 		for i=1:num
