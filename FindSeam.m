95ae8e64d687ab2e33dd4cfce6f9b2df93314ad4
ChuckLeeQB7 <chuckl0416@yahoo.com.tw>
2015-06-27 23:47:10 +0800

    Updated to row+col

--- FindSeam.m
+++ FindSeam.m
@@ -1,62 +1,145 @@
-function [vector] = FindSeam(I, Gimg)
+function [vector] = FindSeam(I, Gimg, decide, test)
 	%Find a seam in img I
-	%where the energy is sepcified by Gimg
+	%where the energy is specified by Gimg
 
 	%calculating image gradient as an energy image
 [rmax, cmax, tmp] = size(I);
 ImgOut = I;
 
-vector = zeros(rmax, 1);
-	%calculating sum of pixels value in each column
-	test2=zeros(rmax,cmax);
-	for row = 1 :rmax
-		for col = 1:cmax
-			if row == 1
-				test2(row,col)=Gimg(row,col);
+    if(decide == 2)
+        vector = zeros(cmax, 1);
+        testcol = test;
+        mincol = min(testcol(:,cmax));
+        locations=find(testcol(:,cmax)==mincol);
+        [y,x]=size(locations);
+	%back tracking to find the seam
+		for loc=1:y
+		j = locations(loc,1);
+		for col=cmax:-1:2
+			if col==cmax
+	%		    ImgOut(row,j,1)=255;
+	%		    ImgOut(row,j,2)=0;
+	%		    ImgOut(row,j,3)=0;
+				testcol(j,col) = realmax;
+				vector(col) = j;
 			end
-			if row>1
-				if col ==1
-					tmp=[test2(row-1,col),test2(row-1,col+1)];
-					test2(row,col)= Gimg(row,col)+min(tmp);
+			%if row < rmax
+				if j==1
+					j2 = j+1;
+					while testcol(j+1,col-1)==realmax && j2<rmax
+						j2=j2+1;
+					end
+					tmp=[testcol(j,col-1),testcol(j2,col-1)];
+					[C,index]=min(tmp);
+					if index==1
+	%		            ImgOut(row-1,j,1)=255;
+	%		            ImgOut(row-1,j,2)=0;
+	%		            ImgOut(row-1,j,3)=0;
+						testcol(j,col-1) = realmax;
+						vector(col-1) = j;
+					end
+					if index==2
+	%		            ImgOut(row-1,j+1,1)=255;
+	%		            ImgOut(row-1,j+1,2)=0;
+	%		            ImgOut(row-1,j+1,3)=0;
+						j=j2;
+						vector(col-1) = j;
+						testcol(j,col-1) = realmax;
+					end
 				end
-				if col>1 && col<cmax
-					tmp1=[test2(row-1,col),test2(row-1,col+1),test2(row-1,col-1)];
-					test2(row,col)= Gimg(row,col)+min(tmp1);
+				if j>1 && j<rmax
+					j1 = j-1;
+					j2 = j+1;
+					while testcol(j1,col-1) == realmax && j1>1
+						j1 = j1-1;
+					end
+					while testcol(j+1,col-1)==realmax && j2<rmax
+						j2=j2+1;
+					end
+					tmp1=[testcol(j,col-1),testcol(j2,col-1),testcol(j1,col-1)];
+					[C,index]=min(tmp1);
+					if index==1
+	%		            ImgOut(row-1,j,1)=255;
+	%		            ImgOut(row-1,j,2)=0;
+	%		            ImgOut(row-1,j,3)=0;
+						testcol(j,col-1) = realmax;
+						vector(col-1) = j;
+					end
+					if index==2
+	%		            ImgOut(row-1,j+1,1)=255;
+	%		            ImgOut(row-1,j+1,2)=0;
+	%		            ImgOut(row-1,j+1,3)=0;
+						j=j2;
+						vector(col-1) = j;
+						testcol(j,col-1) = realmax;
+					end
+					if index==3
+	%		            ImgOut(row-1,j-1,1)=255;
+	%		            ImgOut(row-1,j-1,2)=0;
+	%		            ImgOut(row-1,j-1,3)=0;
+						j=j1;
+						vector(col-1) = j;
+						testcol(j,col-1) = realmax;
+					end
 				end
-				if col == cmax
-					tmp2=[test2(row-1,col),test2(row-1,col-1)];
-					test2(row,col)= Gimg(row,col)+min(tmp2);
+				if j == rmax
+					j1 = j-1;
+					while testcol(j1,col-1) == realmax && j1>1
+						j1 = j1-1;
+					end
+					tmp2=[testcol(j,col-1),testcol(j1,col-1)];
+					[C,index]=min(tmp2);
+					if index==1
+	%		            ImgOut(row-1,j,1)=255;
+	%		            ImgOut(row-1,j,2)=0;
+	%		            ImgOut(row-1,j,3)=0;
+						vector(col-1) = j;
+						testcol(j,col-1) = realmax;
+					end
+					if index==2
+	%		            ImgOut(row-1,j-1,1)=255;
+	%		            ImgOut(row-1,j-1,2)=0;
+	%		            ImgOut(row-1,j-1,3)=0;
+						j=j1;
+						vector(col-1) = j;
+						testcol(j,col-1) = realmax;
+					end
 				end
-			end
+			%end
 		end
 	end
-
-	minval=min(test2(rmax,:));
-	locations=find(test2(rmax,:)==minval);
-	[~, loc]=size(locations);
-	%back traking to find the seam
+    end
+    
+    if(decide == 1)
+        vector = zeros(rmax, 1);
+        testrow = test;
+        minrow = min(testrow(rmax,:));
+        locations=find(testrow(rmax,:)==minrow);
+        [x,y]=size(locations);
+	%back tracking to find the seam
+		for loc=1:y
 		j = locations(1,loc);
 		for row=rmax:-1:2
 			if row==rmax
 	%		    ImgOut(row,j,1)=255;
 	%		    ImgOut(row,j,2)=0;
 	%		    ImgOut(row,j,3)=0;
-				test2(row,j) = realmax;
+				testrow(row,j) = realmax;
 				vector(row) = j;
 			end
 			%if row < rmax
 				if j==1
 					j2 = j+1;
-					while test2(row-1,j+1)==realmax && j2<cmax
+					while testrow(row-1,j+1)==realmax && j2<cmax
 						j2=j2+1;
 					end
-					tmp=[test2(row-1,j),test2(row-1,j2)];
+					tmp=[testrow(row-1,j),testrow(row-1,j2)];
 					[C,index]=min(tmp);
 					if index==1
 	%		            ImgOut(row-1,j,1)=255;
 	%		            ImgOut(row-1,j,2)=0;
 	%		            ImgOut(row-1,j,3)=0;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 						vector(row-1) = j;
 					end
 					if index==2
@@ -65,25 +148,25 @@ vector = zeros(rmax, 1);
 	%		            ImgOut(row-1,j+1,3)=0;
 						j=j2;
 						vector(row-1) = j;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 					end
 				end
 				if j>1 && j<cmax
 					j1 = j-1;
 					j2 = j+1;
-					while test2(row-1,j1) == realmax && j1>1
+					while testrow(row-1,j1) == realmax && j1>1
 						j1 = j1-1;
 					end
-					while test2(row-1,j+1)==realmax && j2<cmax
+					while testrow(row-1,j+1)==realmax && j2<cmax
 						j2=j2+1;
 					end
-					tmp1=[test2(row-1,j),test2(row-1,j2),test2(row-1,j1)];
+					tmp1=[testrow(row-1,j),testrow(row-1,j2),testrow(row-1,j1)];
 					[C,index]=min(tmp1);
 					if index==1
 	%		            ImgOut(row-1,j,1)=255;
 	%		            ImgOut(row-1,j,2)=0;
 	%		            ImgOut(row-1,j,3)=0;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 						vector(row-1) = j;
 					end
 					if index==2
@@ -92,7 +175,7 @@ vector = zeros(rmax, 1);
 	%		            ImgOut(row-1,j+1,3)=0;
 						j=j2;
 						vector(row-1) = j;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 					end
 					if index==3
 	%		            ImgOut(row-1,j-1,1)=255;
@@ -100,22 +183,22 @@ vector = zeros(rmax, 1);
 	%		            ImgOut(row-1,j-1,3)=0;
 						j=j1;
 						vector(row-1) = j;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 					end
 				end
 				if j == cmax
 					j1 = j-1;
-					while test2(row-1,j1) == realmax && j1>1
+					while testrow(row-1,j1) == realmax && j1>1
 						j1 = j1-1;
 					end
-					tmp2=[test2(row-1,j),test2(row-1,j1)];
+					tmp2=[testrow(row-1,j),testrow(row-1,j1)];
 					[C,index]=min(tmp2);
 					if index==1
 	%		            ImgOut(row-1,j,1)=255;
 	%		            ImgOut(row-1,j,2)=0;
 	%		            ImgOut(row-1,j,3)=0;
 						vector(row-1) = j;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 					end
 					if index==2
 	%		            ImgOut(row-1,j-1,1)=255;
@@ -123,11 +206,14 @@ vector = zeros(rmax, 1);
 	%		            ImgOut(row-1,j-1,3)=0;
 						j=j1;
 						vector(row-1) = j;
-						test2(row-1,j) = realmax;
+						testrow(row-1,j) = realmax;
 					end
 				end
 			%end
+		end
 	end
+    end
+	
 %imshow(ImgOut);
 %imshow(I);
 %imwrite(ImgOut, 'b.png');
