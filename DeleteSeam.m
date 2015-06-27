8ee6df9bcdff9d74294623eb662ac8ba90551418
ChuckLeeQB7 <chuckl0416@yahoo.com.tw>
2015-06-27 23:44:45 +0800

    Updated to row+col

--- DeleteSeam.m
+++ DeleteSeam.m
@@ -1,14 +1,26 @@
-function img = DeleteSeam(vector, I)
+function img = DeleteSeam(vector, I, decide)
 	%delete a seam in I specify by vector
 	
 	[rmax, cmax, tmp] = size(I);
-	[rmax, ~] = size(vector);
-	for row=1:rmax
-		[col, index] = max(vector(row, :));
-		I(row, col:cmax-1,:) = I(row, col+1:cmax,:);
-		vector(row, index) = 0;
-	end
-	I(:, cmax, :) = [];
-	cmax = cmax-1;
+    if(decide == 2)
+        cmax = size(vector);
+		for col=1:cmax
+			[row, index] = max(vector(col,: ));
+			I(row:rmax-1, col,:) = I(row+1:rmax, col,:);
+			vector(col, index) = 0;
+		end
+		I(rmax, :, :) = [];
+    end
+	
+    if(decide == 1)
+        rmax = size(vector);
+        for row=1:rmax
+			[col, index] = max(vector(row, :));
+			I(row, col:cmax-1,:) = I(row, col+1:cmax,:);
+			vector(row, index) = 0;
+		end
+		I(:, cmax, :) = [];
+    end
+
 	img = I;
 end
