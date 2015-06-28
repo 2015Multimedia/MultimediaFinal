bb65075729862a996ec6ba1baa0d77d46ac36ceb
assam1231 <assam1231@gmail.com>
2015-06-28 23:11:34 +0800

    Create GUI1.m

---
+++ GUI1.m
@@ -0,0 +1,349 @@
+function varargout = GUI1(varargin)
+% GUI1 MATLAB code for GUI1.fig
+%      GUI1, by itself, creates a new GUI1 or raises the existing
+%      singleton*.
+%
+%      H = GUI1 returns the handle to a new GUI1 or the handle to
+%      the existing singleton*.
+%
+%      GUI1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI1.M with the given input arguments.
+%
+%      GUI1('Property','Value',...) creates a new GUI1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before GUI1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to GUI1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help GUI1
+
+% Last Modified by GUIDE v2.5 28-Jun-2015 03:51:55
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @GUI1_OpeningFcn, ...
+                   'gui_OutputFcn',  @GUI1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+global Gimg;
+global obj;
+
+
+% --- Executes just before GUI1 is made visible.
+function GUI1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to GUI1 (see VARARGIN)
+
+% Choose default command line output for GUI1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes GUI1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = GUI1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+%read img from text area
+filename = get(handles.edit1,'String');
+img = imread(filename);
+[h, w, ~] = size(img);
+
+
+%axes1.position.width = w;   %bug
+%axes1.position.height = h;  %bug
+
+axes(handles.axes1);
+imshow(img);
+
+%save image to handles
+handles.current_data = img;
+
+%modify width and height of textarea
+[h, w, ~] = size(img);
+set(handles.edit3,'String', w);
+set(handles.edit4,'String', h);
+
+
+%save original size
+handles.currentwidth = w;
+handles.currentheight = h;
+
+w = w/5;
+h = h/5;
+set(handles.axes1,'Position', [12 25 w h]);
+axes(handles.axes1);
+
+%
+global obj;
+obj = false;
+guidata(hObject,handles);
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+%call function
+img = handles.current_data;
+removeObject(img);
+
+%modify width and height of textarea
+img = imread('remove.png');
+[h, w, ~] = size(img);
+set(handles.edit3,'String', w);
+set(handles.edit4,'String', h);
+
+%save original size
+handles.currentwidth = w;
+handles.currentheight = h;
+
+w = w/5;
+h = h/5;
+set(handles.axes1,'Position', [12 25 w h]);
+axes(handles.axes1);
+
+%save image
+handles.current_data = img;
+
+global obj;
+obj = false;
+guidata(hObject,handles);
+
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+w = handles.currentwidth;
+h = handles.currentheight;
+w1 = str2num(get(handles.edit3, 'String'));
+h1 = str2num(get(handles.edit4, 'String'));
+global obj;
+global Gimg;
+%save image to handles
+img = handles.current_data;
+if w1 < w
+    removeSeams(img, abs(w-w1), obj, Gimg);
+    img = imread('remove.png');
+elseif w1 > w
+    insertSeam(img, abs(w1-w), obj, Gimg);
+    img = imread('insert.png');
+end
+
+if h1 < h
+     removeSeams_col(img, abs(h-h1), obj, Gimg);
+     img = imread('remove.png');
+elseif h1 > h
+     insertSeam_col(img, abs(h1-h), obj, Gimg);
+     img = imread('insert.png');
+end
+%save image
+
+handles.current_data = img;
+[h, w, ~] = size(img);
+
+w = w/5;
+h = h/5;
+set(handles.axes1,'Position', [12 25 w h]);
+axes(handles.axes1);
+
+%save size of textarea
+handles.currentwidth = w1;
+handles.currentheight = h1;
+
+guidata(hObject,handles);
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over pushbutton1.
+function pushbutton1_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over pushbutton2.
+function pushbutton2_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- If Enable == 'on', executes on mouse press in 5 pixel border.
+% --- Otherwise, executes on mouse press in 5 pixel border or over pushbutton3.
+function pushbutton3_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in togglebutton2.
+function togglebutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global obj;
+global Gimg;
+% Hint: get(hObject,'Value') returns toggle state of togglebutton2
+if (get(hObject,'Value') == get(hObject,'Max'))
+    obj = true;
+    img = handles.current_data;
+    [Gimg, ~] = identifyObject(img, true);
+else
+    obj = false;
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function togglebutton2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to togglebutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+global obj;
+global Gimg;
+obj = false;
+Gimg = 0;
+
+
+% --- Executes during object creation, after setting all properties.
+function axes1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes1
+set(gcf, 'PaperPositionMode', 'auto');
